c52f52729d0570902fde0ca2849df60d06a65925
abhisekadhikari <89593428+abhisekadhikari@users.noreply.github.com>
2023-11-29 22:56:46 +0530

    Finished Header Part

--- app/src/main/java/com/abhitech/tiptap/MainActivity.kt
+++ app/src/main/java/com/abhitech/tiptap/MainActivity.kt
@@ -3,17 +3,34 @@ package com.abhitech.tiptap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.abhitech.tiptap.ui.theme.TipTapTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MyApp {
+                TopHeader()
+            }
         }
     }
 }
@@ -31,16 +48,46 @@ fun MyApp(content: @Composable () -> Unit) {
 
 // <=========
 
+@Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 200.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        // OR
+        //.clip(shape = RoundedCornerShape(CornerSize(12.dp))),
+        color = Color(229, 210, 246, 255)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Monospace
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.displaySmall,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+    }
+}
 
 
-
-
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TipTapTheme {
         MyApp {
-            Text(text = "Hello World!")
+
         }
     }
 }
