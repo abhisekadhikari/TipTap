6417a5115629ad291ab96452e6fcca40e4a38916
abhisekadhikari <89593428+abhisekadhikari@users.noreply.github.com>
2023-11-30 23:41:22 +0530

    adding text field

--- app/src/main/java/com/abhitech/tiptap/MainActivity.kt
+++ app/src/main/java/com/abhitech/tiptap/MainActivity.kt
@@ -12,18 +12,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.abhitech.tiptap.components.InputField
 import com.abhitech.tiptap.ui.theme.TipTapTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +37,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                TopHeader()
+                Column {
+                    TopHeader()
+                    MainContent()
+                }
             }
         }
     }
@@ -83,28 +92,46 @@ fun TopHeader(totalPerPerson: Double = 200.0) {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview(showBackground = false)
 @Composable
 fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
     Surface(
         modifier = Modifier
             .padding(2.dp)
-            .fillMaxWidth()
-            .height(100.dp),
+            .fillMaxWidth(),
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         color = Color.White,
-        border = BorderStroke(width = 2.dp, color = Color.Red)
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
-
+        Column {
+            InputField(valueState = totalBillState,
+                labelId = "Enter Bill",
+                isSingleLine = true,
+                enabled = true,
+                onAction = KeyboardActions {
+                    if (!validState)
+                        return@KeyboardActions
+                    // ToDo on value change
+                    keyboardController?.hide()
+                })
+        }
     }
 }
 
 //@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TipTapTheme {
-        MyApp {
-
-        }
-    }
-}
+//@Composable
+//fun GreetingPreview() {
+//    TipTapTheme {
+//        MyApp {
+//
+//        }
+//    }
+//}
